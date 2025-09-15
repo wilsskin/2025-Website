8fdcce2f86eedf4e043a8eb1d021272630cda18e
Wilson Skinner <w0skinner@gmail.com>
2025-09-14 22:00:18 -0700

    added resume link component and small about changes

--- assets/index.js
+++ assets/index.js
@@ -1,4 +1,14 @@
 // main.js
 // Add any site-wide JavaScript here
 
+const RESUME_URL = 'https://drive.google.com/file/d/1pKdhFi-PGmU2rsgVO0seI4085QxV0Ibw/view?usp=sharing';
+
+document.addEventListener('DOMContentLoaded', () => {
+  document.querySelectorAll('a[data-resume-link]').forEach(a => {
+    a.setAttribute('href', RESUME_URL);
+    a.setAttribute('target', '_blank');
+    a.setAttribute('rel', 'noopener noreferrer');
+  });
+});
+
 console.log('Site loaded');
